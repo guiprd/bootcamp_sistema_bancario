cd8c1fc66cb6daf4692283a8ef37a704723d6a63
Guilherme Prado <guilhermebgprado@gmail.com>
2023-07-18 23:38:51 -0300

    saque e adaptação para o problema

--- user_interface.py
+++ user_interface.py
@@ -1,7 +1,7 @@
 from time import sleep
 
-
 import variaveis
+
 MENU = '''
 
 Funções disponíveis:
@@ -11,8 +11,8 @@ Funções disponíveis:
     [q] Sair
     => '''
 
-def interactions(conta):
 
+def interactions(conta):
     while variaveis.interaction != "q":
         print(MENU)
         variaveis.interaction = str(input("O que deseja fazer? "))
@@ -21,11 +21,13 @@ def interactions(conta):
             conta.deposito()
         elif variaveis.interaction == "s":
             #  Função de saque
-            print("Saque realizado com sucesso")
+            conta.saque()
 
         elif variaveis.interaction == "e":
             # Função de extrato
-            print("Extrato:")
+            conta.historico.imprime()
+            if len(conta.historico.transacoes):
+                print("Saldo: %.2f" % conta.saldo())
 
         elif variaveis.interaction == "q":
             sleep(1)
